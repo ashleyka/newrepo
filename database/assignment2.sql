ab55d77ffdd7a6fe9920405bd4bf3648f76dfe1a
ashleyka <ashleykamujoma@gmail.com>
2024-05-11 05:18:55 +0200

    update database

--- database/assignment2.sql
+++ database/assignment2.sql
@@ -1,24 +1,23 @@
-INSERT INTO account (first_name, last_name, email, password, account_type)
-VALUES ('Tony', 'Stark', 'tony@starkent.com', 'Iam1ronM@n', 'Customer');
+INSERT INTO public.account (account_firstname, account_lastname, account_email, account_password)
+	VALUES ('Tony', 'Stark', 'tony@starkent.com', 'Iam1ronM@n');
 
-
-UPDATE account
+UPDATE public.account
 SET account_type = 'Admin'
-WHERE first_name = 'Tony' AND last_name = 'Stark';
+WHERE account_firstname = 'Tony' AND account_lastname = 'Stark';
 
-DELETE FROM account
-WHERE first_name = 'Tony' AND last_name = 'Stark';
+DELETE FROM public.account
+WHERE account_firstname = 'Tony' AND account_lastname = 'Stark';
 
-UPDATE "schema_name"."inventory"
-SET inv_description = REPLACE(inv_description, 'small interiors', 'a huge interior')
-WHERE inv_make = 'GM' AND inv_model = 'Hummer';
+UPDATE public.inventory
+SET inv_description = REPLACE(inv_description, 'small interiors', 'huge interior')
+WHERE inv_id = 10 AND inv_make = 'GM' AND inv_model = 'Hummer';
 
+SELECT inv_make, inv_model, classification_name
+FROM public.inventory AS inv
+INNER JOIN public.classification AS cla
+ON inv.classification_id = cla.classification_id
+WHERE classification_name = 'Sport';
 
-SELECT i.inv_make, i.inv_model, c.class_name
-FROM inventory AS i
-INNER JOIN classification AS c ON i.inv_class_id = c.class_id
-WHERE c.class_name = 'Sport';
-
-UPDATE inventory
-SET inv_image = REPLACE(inv_image, '/images/', '/images/vehicles/'),
-    inv_thumbnail = REPLACE(inv_thumbnail, '/images/', '/images/vehicles/');
+UPDATE public.inventory
+SET inv_thumbnail = REPLACE(inv_thumbnail, '/images/', '/images/vehicles/'),
+    inv_image = REPLACE(inv_image, '/images/', '/images/vehicles/');
